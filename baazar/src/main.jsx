a006136e93ada206a34c522f27755525a94043ef
ANISH ANAND <165990614+Anishhhh12@users.noreply.github.com>
2026-03-19 14:43:48 +0530

    Update main.jsx

--- baazar/src/main.jsx
+++ baazar/src/main.jsx
@@ -1,16 +1,18 @@
-// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 import { CartProvider } from "./context/CartContext";
-import { UserProvider } from "./context/UserContext"; // ✅ ADD THIS
+import { UserProvider } from "./context/UserContext";
+import { WishlistProvider } from "./context/WishlistContext"; // ADD THIS
 
 createRoot(document.getElementById("root")).render(
-    <UserProvider>        {/* ✅ MUST wrap App */}
+  <UserProvider>
+    <WishlistProvider>          {/* ADD THIS */}
       <CartProvider>
         <App />
       </CartProvider>
-    </UserProvider>
+    </WishlistProvider>          {/* ADD THIS */}
+  </UserProvider>
 );
